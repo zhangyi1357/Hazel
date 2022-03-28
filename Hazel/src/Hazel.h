a81e296dd75453560ffb0575a6f15467d0c903e8
Zhang Yi <zhangyi_1357@sjtu.edu.cn>
2022-03-29 00:22:42 +0800

    ep05 entry point
    add gitigore file and entry point code

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// for use by Hazel Applications
+
+#include "Hazel/Application.h"
+
+
+// -------Entry Point----------------------
+#include "Hazel/EntryPoint.h"
+// ----------------------------------------
