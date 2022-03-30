63cf85a083ede0ecfa936f52d430e4bc64b6c39e
Zhang Yi <zhangyi_1357@sjtu.edu.cn>
2022-03-30 12:04:51 +0800

    ep10 add precompiled header

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,18 +1,15 @@
+#include "hzpch.h"
 #include "Application.h"
 
 #include "Hazel/Event/ApplicationEvent.h"
 #include "Hazel/Log.h"
 
 namespace Hazel {
-    Application::Application()
-    {
-    }
+    Application::Application() {}
 
-    Application::~Application()
-    {
-    }
-    void Application::Run()
-    {
+    Application::~Application() {}
+
+    void Application::Run() {
         WindowResizeEvent e(1280, 720);
         if (e.IsInCategory(EventCategoryApplication)) {
             HZ_TRACE(e);
